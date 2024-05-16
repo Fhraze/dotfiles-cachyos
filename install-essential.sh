ed73c295bcf1dda0f0cb4979651bc96d069cde7a
Frey <bcfrazao@gmail.com>
2024-05-16 15:08:33 -0300

    Update install-essential.sh (published hyprnome package to the AUR)

--- install-essential.sh
+++ install-essential.sh
@@ -1,14 +1,3 @@
-_isInstalled() {
-    package="$1";
-    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-    if [ -n "${check}" ] ; then
-        echo 0; #'0' means 'true' in Bash
-        return; #true
-    fi;
-    echo 1; #'1' means 'false' in Bash
-    return; #false
-}
-
 sudo pacman -Syu
 
 sudo pacman -S \
@@ -21,7 +10,6 @@ ttf-sazanami \
 ttf-baekmuk \
 ttf-hannom \
 opendoas \
-rustup \
 --needed --noconfirm
 
 paru -S \
@@ -29,31 +17,8 @@ ttf-twemoji-color \
 ttf-ancient-fonts \
 ttf-ubraille \
 wofi-emoji \
+hyprnome \
 --needed --noconfirm
-
-rustup default stable
-
-package="hyprnome"
-if [[ $(_isInstalled "${package}") == 1 ]]; then
-  echo "$package not installed. Building $package..."
-  git clone https://github.com/donovanglover/hyprnome -b 0.2.0
-  cd hyprnome
-  makepkg -si
-  cd ..
-  rm -rf hyprnome
-fi
-
-if [[ $(_isInstalled "${package}") == 0 ]]; then
-  echo "$package installed!"
-else
-  echo "Failed to install $package :/, you'll have to install it on your own."
-fi
-
-read -p "$*Do you want to remove rust? [y/n]: " yn
-case $yn in
-  [Yy]*) rustup self uninstall ;;
-  [Nn]*) ;;
-esac
 
 # Setting up SDDM theme
 sudo pacman -S cachyos-themes-sddm --needed --noconfirm
