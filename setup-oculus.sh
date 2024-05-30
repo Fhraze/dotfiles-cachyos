5a6c8529379621a000c938428bd6be831ffd1123
Fhraze <bcfrazao@gmail.com>
2024-05-30 19:21:04 -0300

    ALVR working again!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- setup-oculus.sh
+++ setup-oculus.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syu
-sudo pacman -S corectrl unzip tar android-tools --needed --noconfirm
+sudo pacman -S openxr corectrl unzip tar android-tools --needed --noconfirm
+paru -S alvr-git --needed --noconfirm
 paru -S sidequest-bin --needed --noconfirm
 sudo mkdir -p /etc/udev/rules.d
 if grep -q "SUBSYSTEM" /etc/udev/rules.d/50-oculus.rules; then
@@ -16,5 +17,3 @@ cd ~/ALVR
 wget https://github.com/alvr-org/ADBForwarder/releases/download/v1.3.1/ADBForwarder-linux-x64.zip
 unzip ADBForwarder-linux-x64.zip
 rm ADBForwarder-linux-x64.zip
-
-echo "Now run alvr-install-amd.sh or alvr-install-nvidia.sh (or install from the AUR) and download the apk"
