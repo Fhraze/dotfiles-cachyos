21110b65077342839b601682579fe2e7fea6abe9
Fhraze <bcfrazao@gmail.com>
2024-06-26 16:32:24 -0300

    update setup-twemoji script

--- setup-twemoji.sh
+++ setup-twemoji.sh
@@ -4,3 +4,4 @@ sudo pacman -Rd --nodeps noto-fonts-emoji --noconfirm
 mkdir -p ~/.config/fonts
 cp local.conf ~/.config/fonts/
 fc-cache -f -v
+sudo ln -sf /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf
