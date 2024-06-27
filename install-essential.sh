b88ece566e456bb8615188dc71ecec09291cb62a
Fhraze <bcfrazao@gmail.com>
2024-06-26 21:19:51 -0300

    fixed twemoji version on install-essential script

--- install-essential.sh
+++ install-essential.sh
@@ -2,7 +2,7 @@ sudo pacman -Syu
 
 sudo pacman -S thunar thunar-volman fd zoxide wine-ge-custom ttf-sazanami ttf-baekmuk ttf-hannom opendoas --needed
 
-paru -S ttf-twemoji-color ttf-ancient-fonts ttf-ubraille wofi-emoji hyprnome --needed
+paru -S ttf-twemoji ttf-ancient-fonts ttf-ubraille wofi-emoji hyprnome --needed
 
 # Setting up SDDM theme
 sudo pacman -S cachyos-themes-sddm --needed --noconfirm
